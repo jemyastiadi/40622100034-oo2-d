3e2621bf0dd97b158faf1449d69baec9b23f6362
jemyastiadi18 <jemyastiadi77@gmail.com>
2024-05-05 22:45:13 +0700

    commit pertama

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import LeftNav from '@/components/navbar/LeftNav.vue'
+import MhsForm from '@/components/MahasiswaForm.vue'
+import DosenForm from '@/components/DosenForm.vue'
+import RuanganForm from '@/components/RuanganForm.vue'
+</script>
+
+<template>
+  <v-layout class="rounded rounded-md">
+    <v-app-bar title="Aplikasi penjadwalan kuliah"></v-app-bar>
+
+    <LeftNav></LeftNav>
+
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px">
+      <RouterView></RouterView>
+    </v-main>
+  </v-layout>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HomeForm from '../components/HomeForm.vue'
+</script>
+
+<template>
+  <main>
+    <HomeForm> </HomeForm>
+  </main>
+</template>
